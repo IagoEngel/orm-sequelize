4239781edf79ead9855c8d3e8eb9a41095a361ef
Iago Engel Serafim <iagoengel@yahoo.com>
2023-05-31 20:34:40 -0300

    Módulo 2: Escopo de modelo e validações

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -14,14 +14,37 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Pessoas.init({
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        funcaoValidadora: function(dado) {
+          if (dado.length < 3) throw new Error('o campo nome deve ter mais de 3 caracteres')
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        // isEmail: true
+        isEmail: {
+          args: true,
+          msg: 'dado do tipo e-mail inválido'
+        }
+      }
+    },
     role: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'Pessoas',
     paranoid: true,
+    defaultScope: { // com esse escopo padrão, toda requisição do modelo Pessoa vai trazer somente os usuários com ativo = true do banco
+      where: { ativo: true }
+    },
+    scopes: {
+      todos: { where: {} }, // criou-se esse escopo para trazer todos os dados de Pessoas. Mesmo, quando não estejam ativos. Para utilizar ele, é só escrever .scope('nome do escopo') antes do método que faz a requisição no banco (ex: findAll())
+      // etc: {constraint: valor}
+    }
   });
   return Pessoas;
 };
